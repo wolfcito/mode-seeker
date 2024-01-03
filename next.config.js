1996bbb4bc860bf5b722b364eab2998a674e8e15
Luis Fernando <guffenix@gmail.com>
2024-01-03 00:13:58 -0500

    chore: fleek config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   reactStrictMode: true,
   typescript: {
     ignoreBuildErrors: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
@@ -12,6 +13,9 @@ const nextConfig = {
   webpack: config => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
     return config;
+  },
+  images: {
+    unoptimized: true,
   },
 };
 
